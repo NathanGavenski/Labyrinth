25e129c54e87bc38a235b5152cd98a5d42d67d5c
Nathan Gavenski <nathangavenski@gmail.com>
2021-12-06 16:33:36 -0300

    Create script for generating mazes

---
+++ generate.py
@@ -0,0 +1,85 @@
+
+import argparse
+import os
+from os import listdir
+from os.path import isfile, join
+
+import gym
+import environment
+from tqdm import tqdm
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Args for generating multiple mazes."
+    )
+
+    # General
+    parser.add_argument(
+        '--verbose',
+        action='store_true',
+        help='Wheter or not it should show the progress bar when creating mazes'
+    )
+    parser.add_argument(
+        '--train',
+        type=int,
+        default=100,
+        help="How many mazes for the train set"
+    )
+    parser.add_argument(
+        '--eval',
+        type=int,
+        default=100,
+        help="How many mazes for the evaluation set"
+    )
+
+    # Maze specific
+    parser.add_argument(
+        '--width',
+        type=int,
+        default=10,
+        help="Width of the generated maze"
+    )
+    parser.add_argument(
+        '--height',
+        type=int,
+        default=10,
+        help="Height of the generated maze"
+    )
+
+
+def generate(train, eval, shape, verbose=False):
+    env = gym.make('Maze-v0', shape=shape)
+    env.generate(amount=train+eval)
+
+    mypath = f'./environment/mazes/mazes{shape[0]}/'
+    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+    train_data = files[:train]
+    evaluation_data = files[train:]
+    print(f'Train: {len(train_data)}, Eval: {len(evaluation_data)}')
+
+    os.makedirs(f'{mypath}train/')
+    train_data = train_data if not verbose else tqdm(train_data)
+    for f in train_data:
+        os.rename(
+            f'{mypath}{f}',
+            f'{mypath}train/{f}'
+        )
+    os.makedirs(f'{mypath}eval/')
+    evaluation_data = evaluation_data if not verbose else tqdm(evaluation_data)
+    for f in evaluation_data:
+        os.rename(
+            f'{mypath}{f}',
+            f'{mypath}eval/{f}'
+        )
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    generate(
+        train=args.train,
+        eval=args.eval,
+        shape=(args.width, args.height)
+    )
