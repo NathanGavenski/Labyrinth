0ae5a30d7ccf7f251502f34ede94dacb2f6a269d
Nathan Gavenski <nathangavenski@gmail.com>
2021-12-09 13:00:22 -0300

    fix for script

--- scripts/create_dataset.sh
+++ scripts/create_dataset.sh
@@ -1,17 +1,17 @@
 echo "5X5"
-python maze/create_expert.py --path ./maze/environment/mazes/mazes5 --width 5 --height 5 --save_path ./dataset/dataset5
+xvfb-run  -s "-screen 0 1400x900x24" python maze/create_expert.py --path ./maze/environment/mazes/mazes5 --width 5 --height 5 --save_path ./dataset/dataset5
 
 echo "10X10"
-python maze/create_expert.py --path ./maze/environment/mazes/mazes10 --width 10 --height 10 --save_path ./dataset/dataset10
+xvfb-run -a -s "-screen 0 1400x900x24" python maze/create_expert.py --path ./maze/environment/mazes/mazes10 --width 10 --height 10 --save_path ./dataset/dataset10
 
 echo "25X25"
-python maze/create_expert.py --path ./maze/environment/mazes/mazes25 --width 25 --height 25 --save_path ./dataset/dataset25
+xvfb-run -a -s "-screen 0 1400x900x24" python maze/create_expert.py --path ./maze/environment/mazes/mazes25 --width 25 --height 25 --save_path ./dataset/dataset25
 
 echo "50X50"
-python maze/create_expert.py --path ./maze/environment/mazes/mazes50 --width 50 --height 50 --save_path ./dataset/dataset50
+xvfb-run -a -s "-screen 0 1400x900x24" python maze/create_expert.py --path ./maze/environment/mazes/mazes50 --width 50 --height 50 --save_path ./dataset/dataset50
 
 echo "75X75"
-python maze/create_expert.py --path ./maze/environment/mazes/mazes75 --width 75 --height 75 --save_path ./dataset/dataset75
+xvfb-run -a -s "-screen 0 1400x900x24" python maze/create_expert.py --path ./maze/environment/mazes/mazes75 --width 75 --height 75 --save_path ./dataset/dataset75
 
 echo "100X100"
-python maze/create_expert.py --path ./maze/environment/mazes/mazes100 --width 100 --height 100 --save_path ./dataset/dataset100
+xvfb-run -a -s "-screen 0 1400x900x24" python maze/create_expert.py --path ./maze/environment/mazes/mazes100 --width 100 --height 100 --save_path ./dataset/dataset100
