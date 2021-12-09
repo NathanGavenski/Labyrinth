10e9bb8fac492b710485fd336d3031979b8c720a
Nathan Gavenski <nathangavenski@gmail.com>
2021-12-09 13:13:35 -0300

    create script for random dataset

---
+++ scripts/create_random.sh
@@ -0,0 +1,6 @@
+xvfb-run  -s "-screen 0 1400x900x24" python maze/create_random.py --path ./maze/environment/mazes/mazes5 --width 5 --height 5 --save_path ./dataset/random_dataset5 --amount 10000
+xvfb-run  -s "-screen 0 1400x900x24" python maze/create_random.py --path ./maze/environment/mazes/mazes10 --width 10 --height 10 --save_path ./dataset/random_dataset10 --amount 10000
+xvfb-run  -s "-screen 0 1400x900x24" python maze/create_random.py --path ./maze/environment/mazes/mazes25 --width 25 --height 25 --save_path ./dataset/random_dataset25 --amount 10000
+xvfb-run  -s "-screen 0 1400x900x24" python maze/create_random.py --path ./maze/environment/mazes/mazes50 --width 50 --height 50 --save_path ./dataset/random_dataset50 --amount 10000
+xvfb-run  -s "-screen 0 1400x900x24" python maze/create_random.py --path ./maze/environment/mazes/mazes75 --width 75 --height 75 --save_path ./dataset/random_dataset75 --amount 10000
+xvfb-run  -s "-screen 0 1400x900x24" python maze/create_random.py --path ./maze/environment/mazes/mazes100 --width 100 --height 100 --save_path ./dataset/random_dataset100 --amount 10000
