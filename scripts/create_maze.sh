08d27ddd719d47786916d44f85a8f906608840a9
Nathan Gavenski <nathangavenski@gmail.com>
2021-12-09 09:42:56 -0300

    Create setup script

---
+++ scripts/create_maze.sh
@@ -0,0 +1,17 @@
+echo "5X5"
+python maze/generate.py --path --width 5 --height 5
+
+echo "10X10"
+python maze/generate.py --path --width 10 --height 10
+
+echo "25X25"
+python maze/generate.py --path --width 25 --height 25
+
+echo "50X50"
+python maze/generate.py --path --width 50 --height 50
+
+echo "75X75"
+python maze/generate.py --path --width 75 --height 75
+
+echo "100X100"
+python maze/generate.py --path --width 100 --height 100
