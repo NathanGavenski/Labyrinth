c290f7b917370bb4efdc58ff9208cfbecd71db7e
Nathan Gavenski <nathangavenski@gmail.com>
2021-12-09 09:00:02 -0300

    Create RandomDataset
    Renaming due to random being a reserved word for numpy and to maintain convention.

---
+++ algo/datasets/random_data.py
@@ -0,0 +1,55 @@
+import numpy as np
+from numpy.random.mtrand import shuffle
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torchvision
+
+
+class RandomDataset(Dataset):
+    def __init__(self, path: str, data: np.ndarray) -> None:
+        super().__init__()
+        self.path = path
+        self.data = data
+
+    def __len__(self) -> int:
+        return self.data.shape[0]
+
+    def __getitem__(self, index):
+        _, _, state, action, next_state = self.data[index, :]
+        state_image = torchvision.transforms.ToTensor()(
+            np.load(f'{self.path}{int(state)}.npy', allow_pickle=True)
+        )
+        next_state_image = torchvision.transforms.ToTensor()(
+            np.load(f'{self.path}{int(next_state)}.npy', allow_pickle=True)
+        )
+        action = torch.tensor(action.astype(int))
+        return state_image, next_state_image, action
+
+
+def get_dataloader(path:str, split:float, batch_size : int = 32) -> DataLoader:
+    if split > 1 or split < 0:
+        raise Exception('Split should be between 0 and 1')
+
+    dataset = np.load(f'{path}dataset.npy', allow_pickle=True)
+    if split > 0:
+        idx = int(dataset.shape[0] * split)
+        train = dataset[:idx]
+        eval = dataset[idx:]
+    else:
+        train = dataset
+        eval = None
+
+    train_dataloader = DataLoader(
+        RandomDataset(path, train),
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    eval_dataloader = DataLoader(
+        RandomDataset(path, eval),
+        batch_size=batch_size,
+        shuffle=True
+    ) if split > 0 else None
+
+    return train_dataloader, eval_dataloader
