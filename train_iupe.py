8228414d5c6a9964dda24d5e7cd732f94a55d93e
Nathan Gavenski <nathangavenski@gmail.com>
2022-01-12 14:46:11 -0300

    added multiple runs

--- train_iupe.py
+++ train_iupe.py
@@ -4,16 +4,19 @@ from maze import environment
 from algo.il import IUPE
 
 if __name__ == '__main__':
-    env = gym.make('Maze-v0', shape=(5, 5))
-    algo = IUPE(
-        environment=env,
-        maze_path='./maze/environment/mazes/mazes5/',
-        width=5,
-        height=5,
-        random_dataset='./dataset/random_dataset5/',
-        expert_dataset='./dataset/dataset5/',
-        device='cuda',
-        batch_size=4,
-        verbose=True,
-    )
-    algo.learn(1)
+    for i in range(10):
+        env = gym.make('Maze-v0', shape=(5, 5))
+        algo = IUPE(
+            environment=env,
+            maze_path='./maze/environment/mazes/mazes5/',
+            width=5,
+            height=5,
+            random_dataset='./dataset/random_dataset5/',
+            expert_dataset='./dataset/dataset5/',
+            device='cuda',
+            batch_size=4,
+            verbose=True,
+	    early_stop=True,
+            name=f'5x5-{i}'
+        )
+        algo.learn(100)
