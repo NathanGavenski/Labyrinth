c16020ffe83a7c81503054985fa0dfaf72ab7fc4
Nathan Gavenski <nathangavenski@gmail.com>
2022-01-20 18:50:51 -0300

    Testing ILPO

--- algo/il/ILPO.py
+++ algo/il/ILPO.py
@@ -1,12 +1,15 @@
 """ILPO network for images."""
 import collections
+from collections import deque
 import glob
 import math
 import os
 import random
 import time
-import json
+from os import listdir
+from os.path import isfile, join
 
+import cv2
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
@@ -70,7 +73,7 @@ def deprocess(image):
         # [-1, 1] => [0, 1]
         return (image + 1) / 2
 
-class ILPO:
+class ImageILPO:
     def __init__(
         self, 
         input_dir, 
@@ -550,6 +553,262 @@ class ILPO:
         examples = self.load_examples()
         self.train_examples(examples)
 
+class Policy(ImageILPO):
+    def __init__(
+        self,
+        sess,
+        shape,
+        checkpoint,
+        game,
+        maze_path,
+        ngf=128,
+        batch_size=4,
+        n_actions=4,
+        real_actions=4,
+        policy_lr=0.001,
+        verbose=True,
+        use_encoding=False,
+        experiment=False,
+        exp_writer=None
+    ):
+        """Initializes the ILPO policy network."""
+
+        mypath = f'{maze_path}train/'
+        mazes = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
+        self.mazes = np.array(mazes)
+
+        self.sess = sess
+        self.verbose = verbose
+        self.use_encoding = use_encoding
+        self.inputs = tf.placeholder("float", shape)
+        self.targets = tf.placeholder("float", shape)
+        self.state = tf.placeholder("float", shape)
+        self.action = tf.placeholder("int32", [None])
+        self.fake_action = tf.placeholder("int32", [None])
+        self.reward = tf.placeholder("float", [None])
+        self.exp_writer = exp_writer
+        self.experiment = experiment
+        self.policy_lr = policy_lr
+        self.checkpoint = checkpoint
+        self.n_actions = n_actions
+        self.real_actions = real_actions
+        self.game = game
+        self.batch_size = batch_size
+        self.ngf = ngf
+
+        processed_inputs = self.process_inputs(self.inputs)
+        processed_targets = self.process_inputs(self.targets)
+        processed_state = self.process_inputs(self.state)
+
+        self.model = self.create_model(processed_inputs, processed_targets)
+
+        self.state_encoding = self.encode(processed_inputs)[-1]
+        self.action_label, self.loss = self.action_remap_net(processed_state, self.action, self.fake_action)
+        self.loss_summary = tf.summary.scalar("policy_loss", tf.squeeze(self.loss))
+
+        if not self.experiment:
+            self.reward_summary = tf.summary.scalar("reward", tf.squeeze(self.reward[0]))
+            self.summary_writer = tf.summary.FileWriter("policy_logs", graph=tf.get_default_graph())
+
+        self.train_step = tf.train.AdamOptimizer(self.policy_lr).minimize(self.loss)
+
+        ilpo_var_list = []
+        policy_var_list = []
+
+        # Restore ILPO params and initialize policy params.
+        for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
+            if "ilpo" in var.name:
+                ilpo_var_list.append(var)
+            else:
+                policy_var_list.append(var)
+
+        saver = tf.train.Saver(var_list=ilpo_var_list)
+        checkpoint = tf.train.latest_checkpoint(self.checkpoint)
+        saver.restore(sess, checkpoint)
+        sess.run(tf.variables_initializer(policy_var_list))
+
+        self.deprocessed_outputs = [tf.image.convert_image_dtype(deprocess(output), dtype=tf.uint8, saturate=True) for output in self.model.outputs]
+        #self.viewer = rendering.SimpleImageViewer()
+
+    def encode(self, state):
+        """Runs an encoding on a state."""
+
+        with tf.variable_scope("ilpo_loss", reuse=True):
+            with tf.variable_scope("state_encoding"):
+                return self.create_encoder(state)
+
+    def min_action(self, state, action, next_state):
+        """Find the minimum action for training."""
+
+        # Given state and action, find the closest predicted next state to the real one.
+        # Use the real action as a training label for remapping the action label.
+        fake_next_states = self.sess.run(self.deprocessed_outputs, feed_dict={self.inputs: [state]})
+
+        if self.use_encoding:
+            next_state_encoding = self.sess.run(self.state_encoding, feed_dict={self.inputs: [next_state]})
+            fake_state_encodings = [self.sess.run(
+                self.state_encoding,
+                feed_dict={self.inputs: fake_next_state}) for fake_next_state in fake_next_states]
+            distances = [np.linalg.norm(next_state_encoding - fake_state_encoding) for fake_state_encoding in fake_state_encodings]
+        else:
+            distances = [np.linalg.norm(next_state - fake_next_state) for fake_next_state in fake_next_states]
+        min_action = np.argmin(distances)
+        min_state = fake_next_states[min_action][0]
+
+        if self.verbose:
+            display_states = [cv2.cvtColor(cv2.resize(fake_next_state[0], (128, 128)), cv2.COLOR_RGB2BGR) for fake_next_state in fake_next_states]
+            cv2.imshow("outputs", np.hstack(display_states))
+            cv2.imshow("state", cv2.cvtColor(cv2.resize(state, (128, 128)), cv2.COLOR_RGB2BGR))
+            cv2.imshow("NextPrediction", np.hstack([
+                cv2.cvtColor(cv2.resize(next_state, (128, 128)), cv2.COLOR_RGB2BGR),
+                cv2.cvtColor(cv2.resize(min_state, (128, 128)), cv2.COLOR_RGB2BGR)]))
+            cv2.waitKey(0)
+        return min_action
+
+    def action_remap_net(self, state, action, fake_action):
+        """Network for remapping incorrect action labels."""
+
+       # fake_state_encoding = tf.stop_gradient(slim.flatten(lrelu(self.encode(state)[-1], .2)))
+        with tf.variable_scope("action_remap"):
+            fake_state_encoding = lrelu(slim.flatten(self.create_encoder(state)[-1]), .2)
+            fake_action_one_hot = tf.one_hot(fake_action, self.n_actions)
+            fake_action_one_hot = lrelu(fully_connected(fake_action_one_hot, int(fake_state_encoding.shape[-1])), .2)
+            real_action_one_hot = tf.one_hot(action, self.real_actions, dtype="float32")
+            fake_state_action = tf.concat([fake_state_encoding, fake_action_one_hot], axis=-1)
+            prediction = lrelu(fully_connected(fake_state_action, 64), .2)
+            prediction = lrelu(fully_connected(prediction, 64), .2)
+            prediction = fully_connected(prediction, self.real_actions)
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=real_action_one_hot, logits=prediction))
+
+            return tf.nn.softmax(prediction), loss
+
+    def P(self, state):
+        """Returns the next_state probabilities for a state."""
+
+        return self.sess.run(self.model.actions, feed_dict={self.inputs: [state]})[0]
+
+    def greedy(self, state):
+        """Returns the greedy remapped action for a state."""
+
+        p_state = self.P(state)
+        action = np.argmax(p_state)
+
+        remapped_action = self.sess.run(self.action_label, feed_dict={self.state: [state], self.fake_action: [action]})[0]
+
+        if self.verbose:
+            print(self.P(state))
+            print(remapped_action)
+            print("\n")
+
+        return np.argmax(remapped_action)
+
+    def render(self, obs):
+        pass
+        #self.viewer.imshow(obs)
+
+    def eval_policy(self, game, t):
+        """Evaluate the policy."""
+
+        total_reward = 0
+
+        for evaluation in range(0, 10):
+            terminal = [False]
+            obs = np.squeeze(game.reset())
+            steps = 0
+            print("Evaluating", evaluation)
+
+            while not terminal[0] and steps < 200:
+                obs = np.squeeze(obs)
+                self.render(obs)
+                obs = cv2.resize(obs, (128, 128))
+
+                if np.random.uniform(0,1) <= .9:
+                    action = self.greedy(obs)
+                else:
+                    action = game.action_space.sample()
+
+                for _ in range(0, 1):
+                    obs, reward, terminal, _ = game.step(np.array([action]))
+
+                total_reward += reward
+                steps +=1
+
+            print("Average reward", total_reward / 10.)
+
+        if not self.experiment:
+            reward_summary = self.sess.run([self.reward_summary], feed_dict={self.reward: total_reward / 10.})[0]
+            self.summary_writer.add_summary(reward_summary, t)
+        else:
+            self.exp_writer.write(str(t) + "," + str(total_reward / 10.) + "\n")
+
+    def run_policy(self, times=10):
+        """Run the policy."""
+        mazes = np.repeat(self.mazes, times, axis=0)
+        np.random.shuffle(mazes)
+
+        for idx, maze in enumerate(mazes):
+            terminal = False
+            self.game.reset()
+            self.game.load(maze)
+            obs = self.game.render('rgb_array')
+            obs = np.squeeze(obs)
+            obs = cv2.resize(obs, (128, 128))
+
+            total_reward = 0
+            D = deque()
+            episode = 0
+            epsilon = .2
+
+            prev_obs = obs.copy()
+
+            t = 0
+            while not terminal:           
+                prev_obs = np.copy(obs)
+
+                if np.random.uniform(0,1) > epsilon and t > 0:
+                    action = self.greedy(obs)
+                else:
+                    action = self.game.action_space.sample()
+
+                if epsilon > .2 and t >= 0:
+                    epsilon -= (.2 - .2) / 1000
+
+                obs, reward, terminal, _ = self.game.step(action)
+                obs = self.game.render('rgb_array')
+
+                obs = np.squeeze(obs)
+                obs = cv2.resize(obs, (128, 128))
+                total_reward += reward
+                fake_action = self.min_action(prev_obs, action, obs)
+
+                D.append((prev_obs, action, fake_action))
+
+                if len(D) >= self.batch_size and t > 0:
+                    minibatch = random.sample(D, self.batch_size)
+                    obs_batch = [d[0] for d in minibatch]
+                    action_batch = [d[1] for d in minibatch]
+                    fake_action_batch = [d[2] for d in minibatch]
+
+                    _, loss_summary, loss = self.sess.run([
+                            self.train_step, 
+                            self.loss_summary, 
+                            self.loss
+                        ],
+                            feed_dict = {
+                            self.state: obs_batch,
+                            self.action: action_batch,
+                            self.fake_action: fake_action_batch
+                        }
+                    )
+
+                t += 1
+
+            if t % 200 == 0 and t >= 0:
+                self.eval_policy(self.game, idx)
+                obs = np.squeeze(self.game.reset())
+                obs = cv2.resize(obs, (128, 128))
+
+
 def create_dataset(path, file, output_dir):
     from PIL import Image
     from tqdm import tqdm
@@ -558,6 +817,7 @@ def create_dataset(path, file, output_dir):
         os.makedirs(output_dir)
 
     dataset = np.load(f'{path}/{file}', allow_pickle=True)
+    dataset = np.repeat(dataset, 10, axis=0)
     for idx, data in enumerate(tqdm(dataset)):
         _, _, s, _, ns = data.astype(int)
         state = np.load(f'{path}/{s}.npy')
--- train_ilpo.py
+++ train_ilpo.py
@@ -1,5 +1,10 @@
 from os import path
-from algo.il import ILPO, create_ilpo_dataset
+from algo.il import ImageILPO, PolicyILPO, create_ilpo_dataset
+
+import gym
+import tensorflow as tf
+
+from maze import environment
 
 if __name__ == '__main__':
     if not path.exists('./dataset/ilpo_dataset'):
@@ -9,11 +14,33 @@ if __name__ == '__main__':
             output_dir='./dataset/ilpo_dataset',
         )
 
-    ilpo = ILPO(
+    ilpo = ImageILPO(
         input_dir='./dataset/ilpo_dataset',
         output_dir='./tmp/ilpo/output',
         checkpoint_dir='./tmp/ilpo/checkpoint',
-        batch_size=4,
+        batch_size=1,
         ngf=15,
     )
     ilpo.run()
+
+    # game = gym.make('Maze-v0', shape=(5, 5))
+
+    # config = tf.ConfigProto()
+    # config.gpu_options.allow_growth = True
+    # sess = tf.Session(config=config)
+    
+    # with sess.as_default():
+    #     policy = PolicyILPO(
+    #         sess,
+    #         shape=[None, 128, 128, 3],
+    #         checkpoint='./tmp/ilpo/checkpoint',
+    #         game=game,
+    #         maze_path='./maze/environment/mazes/mazes5/',
+    #         ngf=15,
+    #         verbose=False,
+    #         experiment=True,
+    #         use_encoding=True,
+    #         # save_path='./tmp/ilpot/output',
+    #     )
+
+    #     policy.run_policy(times=1)
