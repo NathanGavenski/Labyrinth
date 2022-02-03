5f1aa9c88ebfbbff2a54747253b9e53bf1beb8bf
Nathan Gavenski <nathangavenski@gmail.com>
2022-02-03 17:07:23 -0300

    going local

--- train_gail.py
+++ train_gail.py
@@ -6,14 +6,17 @@ from algo.il import GAIL, create_gail_dataset
 
 if __name__ == '__main__':
 
-    for idx in range(5):
-        path, file = './dataset/dataset5', 'dataset.npy'
-        dataset = create_gail_dataset(path, file, times=100)
-        env = gym.make('Maze-v0', shape=(5, 5))
+    print('Creating dataset')
+    path, file = './dataset/dataset5', 'dataset.npy'
+    dataset = create_gail_dataset(path, file, times=100)
 
+    for idx in range(5):
+        print(f'Running {idx}')
+        env = gym.make('Maze-v0', shape=(5, 5))
         model = GAIL(
             dataset=dataset,
             game=env,
             maze_path='./maze/environment/mazes/mazes5/',
+            log_name=idx
         )
         model.run()
