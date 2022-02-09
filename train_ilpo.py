0e60a155f8803ad258492c0dad3854198ea36347
Nathan Gavenski <nathangavenski@gmail.com>
2022-02-09 16:54:56 -0300

    Script for test and IUPE fix on typing

--- train_ilpo.py
+++ train_ilpo.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from os import path
 import shutil
@@ -8,29 +9,67 @@ import tensorflow as tf
 from algo.il import ImageILPO, PolicyILPO, create_ilpo_dataset
 from maze import environment
 
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Args for creating expert dataset."
+    )
+
+    # General
+    parser.add_argument(
+        '--times',
+        type=int,
+        default=10,
+        help='How many times should repeat each maze when unbiased is turned on'
+    )
+
+    parser.add_argument(
+        '--idx',
+        type=int,
+    )
+
+    parser.add_argument(
+        '--gpu',
+        type=str,
+        default='-1'
+    )
+    
+    # Maze specific
+    parser.add_argument(
+        '--size',
+        type=int,
+        default=10,
+        help="Width of the generated maze"
+    )
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    return parser.parse_args()
+    
+
 if __name__ == '__main__':
+    args = get_args()
+
     if not path.exists('./dataset/ilpo_dataset'):
         create_ilpo_dataset(
-            path='./dataset/dataset5',
+            path=f'./dataset/dataset{args.size}',
             file='dataset.npy',
             output_dir='./dataset/ilpo_dataset',
         )
 
-    #if os.path.exists('./tmp/ilpo/output/'):
-    #    shutil.rmtree('./tmp/ilpo/output/')
+    if os.path.exists('./tmp/ilpo/output/'):
+       shutil.rmtree('./tmp/ilpo/output/')
 
-    #tf.reset_default_graph()
-    #ilpo = ImageILPO(
-    #    input_dir='./dataset/ilpo_dataset',
-    #    output_dir='./tmp/ilpo/output',
-    #    checkpoint_dir='./tmp/ilpo/output',
-    #    max_steps=None,
-    #    max_epochs=5,
-    #    batch_size=32,
-    #    ngf=15,
-    #)
-    #ilpo.run()
-    #exit()
+    tf.reset_default_graph()
+    ilpo = ImageILPO(
+       input_dir='./dataset/ilpo_dataset',
+       output_dir='./tmp/ilpo/output',
+       checkpoint_dir='./tmp/ilpo/output',
+       max_steps=None,
+       max_epochs=5,
+       batch_size=32,
+       ngf=15,
+    )
+    ilpo.run()
 
     tf.reset_default_graph()
     config = tf.ConfigProto(
@@ -45,13 +84,13 @@ if __name__ == '__main__':
             sess,
             shape=[None, 128, 128, 3],
             checkpoint='./tmp/ilpo/output/',
-            game=gym.make('Maze-v0', shape=(5, 5)),
-            maze_path='./maze/environment/mazes/mazes5/',
+            game=gym.make('Maze-v0', shape=(args.size, args.size)),
+            maze_path=f'./maze/environment/mazes/mazes{args.size}/',
             ngf=15,
             verbose=False,
             experiment=True,
             use_encoding=True,
-            name='test'
+            name=f'{args.size}x{args.size}-{args.idx}'
         )
 
-        policy.run_policy(times=10)
+        policy.run_policy(times=args.times)
--- train_iupe.py
+++ train_iupe.py
@@ -1,22 +1,64 @@
+import argparse
+import os
+
 import gym
 
 from maze import environment
 from algo.il import IUPE
 
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Args for creating expert dataset."
+    )
+
+    # General
+    parser.add_argument(
+        '--times',
+        type=int,
+        default=10,
+        help='How many times should repeat each maze when unbiased is turned on'
+    )
+
+    parser.add_argument(
+        '--idx',
+        type=int,
+    )
+
+    parser.add_argument(
+        '--gpu',
+        type=str,
+        default='-1'
+    )
+    
+    # Maze specific
+    parser.add_argument(
+        '--size',
+        type=int,
+        default=10,
+        help="Width of the generated maze"
+    )
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    for i in range(3, 5):
-        env = gym.make('Maze-v0', shape=(5, 5))
-        algo = IUPE(
-            environment=env,
-            maze_path='./maze/environment/mazes/mazes5/',
-            width=5,
-            height=5,
-            random_dataset='./dataset/random_dataset5/',
-            expert_dataset='./dataset/dataset5/',
-            device='cuda',
-            batch_size=4,
-            verbose=True,
-	    early_stop=True,
-            name=f'5x5-{i}'
-        )
-        algo.learn(100)
+    args = get_args()
+
+    env = gym.make('Maze-v0', shape=(args.size, args.size))
+    algo = IUPE(
+        environment=env,
+        maze_path=f'./maze/environment/mazes/mazes{args.size}/',
+        width=args.size,
+        height=args.size,
+        episode_times=args.times,
+        random_dataset=f'./dataset/random_dataset{args.size}/',
+        expert_dataset=f'./dataset/dataset{args.size}/',
+        device='cuda',
+        batch_size=12,
+        verbose=True,
+        early_stop=True,
+        name=f'{args.size}x{args.size}-{args.idx}'
+    )
+    algo.learn(100)
