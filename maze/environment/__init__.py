d9dc117a37789f2700f998247055f4f9be9f15b2
Nathan <nathangavenski@gmail.com>
2022-12-08 11:15:03 +0000

    update requirements and gym version

--- maze/environment/__init__.py
+++ maze/environment/__init__.py
@@ -4,6 +4,7 @@ register(
     id="Maze-v0",
     entry_point="maze.environment.maze:Maze",
     order_enforce=False,
+    disable_env_checker=True,
     kwargs={'shape': (10, 10), 'occlusion': False}
 )
 
@@ -12,5 +13,6 @@ register(
     entry_point="environment.maze:Maze",
     reward_threshold=1,
     order_enforce=False,
+    disable_env_checker=True,
     kwargs={'shape': (10, 10), 'occlusion': False}
 )
