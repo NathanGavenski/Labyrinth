979b5a9754639fbf9ce226ee69c5c844172be7b0
Nathan Gavenski <nathangavenski@gmail.com>
2025-02-27 14:06:55 +0000

    removing unrelated files

--- convert_structure.py
+++
@@ -1,5 +0,0 @@
-from src.create_maze_from_file import convert_from_file, create_default_maze
-
-if __name__ == "__main__":
-    convert_from_file("structure_test", "./structure_test.txt")
-    #create_default_maze((5, 5), "structure_test.py")
